7502d1a3592848800a1df97fbce6fb1404987111
vikas-saini07 <56854067+vikas-saini07@users.noreply.github.com>
2022-07-26 10:19:07 +1000

    Create createtokenusingmodal_post.php

---
+++ hosted/createtokenusingmodal_post.php
@@ -0,0 +1,74 @@
+<html>
+<head>
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+<script type="text/javascript" src="https://www.simplify.com/commerce/simplify.pay.js"></script>
+<link rel="stylesheet" type="text/css" href="css/hostedchkout/style.css">
+
+
+
+
+<script>
+  /**
+     * Configure hosted payments callback.
+     */
+    var hostedPayments = SimplifyCommerce.hostedPayments(
+        function(response) {
+            var cardToken = response.cardToken;
+            var amount = response.amount;
+            var currency = "<?php echo $_POST['currency'] ?>";
+            var pubkey =  "<?php echo $_POST['pubkey'] ?>";
+            var pvtkey =  "<?php echo $_POST['pvtkey'] ?>";
+            console.log(response);
+            console.log("Card Token: " + cardToken);
+            var ref = "<?php echo $_POST['ref'] ?>"
+            // TODO: Pass token to the server & use Simplify's API to make a payment
+                        completePayload = {
+                                token: response.cardToken,
+                                name: "<?php echo $_POST['name'] ?>",
+                                email: "<?php echo $_POST['email'] ?>",
+                                ref: ref,
+                                amount: amount,
+                                desc: "<?php echo $_POST['desc'] ?>",
+                                currency: currency,
+                                pubkey: pubkey,
+                                pvtkey: pvtkey
+                               
+                        };
+
+                        $.post('../directapi/createpayment_post.php', completePayload, function (completeResponse, status) {
+                                 var response = JSON.parse(completeResponse);
+                                // var responsenew = JSON.stringify(completeResponse);
+                                // console.log("completeResponse: " + completeResponse);
+                                 // console.log("responsenew: " + responsenew);
+                                 console.log("response: " + response);
+                        });
+                        hostedPayments.enablePayBtn();
+
+        },
+        {
+            operation: 'create.token',
+            name: "<?php echo $_POST['name'] ?>",
+            description: "<?php echo $_POST['desc'] ?>",
+            amount: "<?php echo $_POST['amount'] ?>",
+            email: "<?php echo $_POST['email'] ?>",
+            color: "#12B830"
+        }
+    );
+    /**
+     * Handle the error response from the server call that
+     * failed to make a payment from the card token.
+     */
+    function apiPaymentErrorHandler() {
+        // re-enable the payment button, so the user can try again.
+        hostedPayments.enablePayBtn();
+        // TODO: Handle error
+    }
+</script>
+</head>
+<body>
+<div id="display"></div>
+<button data-sc-key="<?php echo $_POST['pubkey'] ?>">
+Pay Now
+</button>
+</body>
+</html>
