ee509838c9eb239fe1e2a08145ce1271fb6152d1
vikas-saini07 <56854067+vikas-saini07@users.noreply.github.com>
2023-01-11 14:46:48 +1000

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -1,17 +1,5 @@
 <?php
 $HTTP_RAW_POST_DATA =file_get_contents('php://input');
 
-require_once("./lib/Simplify.php");
-
-Simplify::$publicKey = 'YOUR_PUBLIC_API_KEY';
-Simplify::$privateKey = 'YOUR_PRIVATE_API_KEY';
-
-$event = Simplify_Event::createEvent(array(
-'payload' => $HTTP_RAW_POST_DATA
-));
-
-$name = $event->name;
-$data = $event->data;
-
-echo $data;
+echo $HTTP_RAW_POST_DATA;
 ?>
